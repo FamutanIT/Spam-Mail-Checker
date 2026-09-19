73bab524f8fe76743ea8377d111fb3b3fc2b981c
FamutanIT <famutan1@gmail.com>
2026-09-19 15:57:00 +0700

    project

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-data = pd.read_csv(r"C:\Users\SV\Desktop\CODE\Second Project\spam.csv")
+#read the file
+data1 = pd.read_csv(r"C:\Users\SV\Desktop\CODE\Second Project\spam.csv")
+data2 = pd.read_csv(r"C:\Users\SV\Desktop\CODE\Second Project\mail_data.csv")
+#combine thease file each file
+data1['spam'] = data1['spam'].replace([0,1] ,['Not Spam', 'Spam',])
+data2['spam'] = data2['Category'].replace(['ham','spam'], ['Not Spam', 'Spam'])
+data2.drop(columns=['Category'], inplace= True)
+data2.rename(columns={'Message':'text'}, inplace=True)
+data = pd.concat([data1,data2], ignore_index=True)
 data.drop_duplicates(inplace=True)
-data['spam'] = data['spam'].replace([0,1],['Not Spam', 'Spam'])
 text = data['text']
 sp = data['spam']
 (text_train, text_test, sp_train, sp_test) = train_test_split(
-    text,sp, test_size=0.2
+    text,sp, test_size=0.2, random_state= 43
 )
 cv = CountVectorizer(stop_words = 'english')
 features = cv.fit_transform(text_train)
@@ -16,7 +23,8 @@ features = cv.fit_transform(text_train)
 model = MultinomialNB()
 model.fit(features, sp_train)
 #test
-features_test = cv.transform(text_test)
+#features_test = cv.transform(text_test)
+#print(model.score(features_test, sp_test))
 #predict
 def predict(sample):
  sample = cv.transform([sample]).toarray()
